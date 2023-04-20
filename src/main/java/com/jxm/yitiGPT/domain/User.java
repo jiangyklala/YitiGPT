e406f7d350f5ca76a2854b710a465bcb5c9b6f98
jiangyk <jiangyk@nwafu.edu.cn>
2023-04-20 10:49:42 +0800

    新增: 根据一定策略进行提问前的扣费

---
+++ src/main/java/com/jxm/yitiGPT/domain/User.java
@@ -0,0 +1,112 @@
+package com.jxm.yitiGPT.domain;
+
+public class User {
+    private Long id;
+
+    private String username;
+
+    private String useraccount;
+
+    private String password;
+
+    private String salt;
+
+    private String githubId;
+
+    private String email;
+
+    private Long balance;
+
+    private Integer type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUseraccount() {
+        return useraccount;
+    }
+
+    public void setUseraccount(String useraccount) {
+        this.useraccount = useraccount;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getGithubId() {
+        return githubId;
+    }
+
+    public void setGithubId(String githubId) {
+        this.githubId = githubId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", username=").append(username);
+        sb.append(", useraccount=").append(useraccount);
+        sb.append(", password=").append(password);
+        sb.append(", salt=").append(salt);
+        sb.append(", githubId=").append(githubId);
+        sb.append(", email=").append(email);
+        sb.append(", balance=").append(balance);
+        sb.append(", type=").append(type);
+        sb.append("]");
+        return sb.toString();
+    }
+}
